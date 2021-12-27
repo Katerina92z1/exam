da4e6f93b5577c7291122e1471b46abcbefb1801
Katerina92z1 <e.katerinka.z@mail.ru>
2021-12-27 14:37:43 +0300

    ))

---
+++ 16.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+
+using namespace std;
+
+long double f(int N)
+{
+    if (N < 0) 
+    {
+        return 0; 
+    }
+    if (N == 0) 
+    {
+        return 1; 
+    }
+    else {
+        return N * f(N - 1); 
+    }
+}
+
+int main()
+{
+    int N;
+    cin >> N;
+    cout << N << " = " << f(N) << endl << endl; // f(N) - функция для вычисления факториала.
+    return 0;
+}
